fcd3f848b325a22c60592dec60d838999768c859
Ntd93 <nguyentiendung2003@gmail.com>
2023-10-04 18:40:48 +0700

    first commit

---
+++ src/main/java/com/ntd/oauth2/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ntd.oauth2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.anyRequest()
+                                .authenticated()
+                ).oauth2Login()
+        ;
+        return http.build();
+    }
+}
